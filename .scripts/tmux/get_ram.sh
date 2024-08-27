a1743f3345509454219bf7a3a0c88337bc240ea1
kmate19 <kissmate019@gmail.com>
2024-08-27 17:53:57 +0200

    fixed ram formatting on linux

--- .scripts/tmux/get_ram.sh
+++ .scripts/tmux/get_ram.sh
@@ -7,7 +7,7 @@ get_ram_usage() {
         used_ram=$(vm_stat | grep 'Pages active\|Pages speculative\|Pages wired\|Pages purgeable' | awk '{a=substr($NF,1,length($NF)-1);} {sum+=a;} END {printf "%.2f", sum*16384/1024/1024/1024}')
         echo "${used_ram}/${total_ram} GB"
   else
-    echo $(free -m | grep "Mem:" | awk '{printf "%d/%d GB", $3 , $2}')
+    echo $(free -m | grep "Mem:" | awk '{printf "%.2f/%.2f GB", $3/1024 , $2/1024}')
   fi
 }
 
