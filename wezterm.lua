7ea33f78674d371770b021a392817c7355dbdfc8
kmate19 <kissmate019@gmail.com>
2024-10-20 19:37:22 +0200

    cm

--- wezterm.lua
+++ wezterm.lua
@@ -1,1 +1,13 @@
-./.config/wezterm/wezterm.lua
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "rose-pine"
+config.font = wezterm.font("Iosevka")
+config.default_domain = "WSL:Arch"
+config.window_background_opacity = 0.7
+config.win32_system_backdrop = "Acrylic"
+config.font_size = 14
+config.audible_bell = "Disabled"
+
+return config
