e87b9d0851fa165057f8686dde236f817bc7d148
kmate19 <kissmate019@gmail.com>
2024-10-21 12:39:19 +0200

    added cross platform code for wezterm, changed font to monaspace neon

--- wezterm.lua
+++ wezterm.lua
@@ -2,12 +2,20 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+if wezterm.target_triple:find("windows") ~= nil then
+	config.default_domain = "WSL:Arch"
+	config.win32_system_backdrop = "Acrylic"
+	config.font_size = 13
+elseif wezterm.target_triple:find("darwin") ~= nil then
+	config.macos_window_background_blur = 60
+	config.font_size = 17
+elseif wezterm.target_triple:find("linux") ~= nil then
+	config.font_size = 13
+end
+
 config.color_scheme = "rose-pine"
-config.font = wezterm.font("Iosevka")
-config.default_domain = "WSL:Arch"
+config.font = wezterm.font("Monaspace Neon")
 config.window_background_opacity = 0.7
-config.win32_system_backdrop = "Acrylic"
-config.font_size = 14
 config.audible_bell = "Disabled"
 
 return config
