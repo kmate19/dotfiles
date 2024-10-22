4a5a72637fd9e74d932e353fe8169b6e4789ad6b
kmate19 <kissmate019@gmail.com>
2024-10-22 11:51:33 +0200

    removed opacity

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 if wezterm.target_triple:find("windows") ~= nil then
 	config.default_domain = "WSL:Arch"
-	config.win32_system_backdrop = "Acrylic"
+	-- config.win32_system_backdrop = "Acrylic"
 	config.font_size = 13
 elseif wezterm.target_triple:find("darwin") ~= nil then
 	config.macos_window_background_blur = 60
@@ -15,7 +15,7 @@ end
 
 config.color_scheme = "gruvbox_dark"
 config.font = wezterm.font("Monaspace Neon")
-config.window_background_opacity = 0.7
+-- config.window_background_opacity = 0.7
 config.audible_bell = "Disabled"
 
 return config
