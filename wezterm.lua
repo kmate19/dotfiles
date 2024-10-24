e8d26e6fc3f07f6c306ab3f147236a9d440d7348
kmate19 <kissmate019@gmail.com>
2024-10-24 18:47:46 +0200

    changed wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -4,18 +4,18 @@ local config = wezterm.config_builder()
 
 if wezterm.target_triple:find("windows") ~= nil then
 	config.default_domain = "WSL:Arch"
-	-- config.win32_system_backdrop = "Acrylic"
-	config.font_size = 13
+	config.win32_system_backdrop = "Acrylic"
+	config.font_size = 11
 elseif wezterm.target_triple:find("darwin") ~= nil then
 	config.macos_window_background_blur = 60
-	config.font_size = 17
+	config.font_size = 14
 elseif wezterm.target_triple:find("linux") ~= nil then
-	config.font_size = 13
+	config.font_size = 11
 end
 
-config.color_scheme = "gruvbox_dark"
-config.font = wezterm.font("Monaspace Neon")
--- config.window_background_opacity = 0.7
+config.color_scheme = "Poimandres"
+-- config.font = wezterm.font("Inconsolata")
+config.window_background_opacity = 0.7
 config.audible_bell = "Disabled"
 
 return config
