8a6e9b3c746d523ee01fedd0b8e05faaab1630f8
kmate19 <kissmate019@gmail.com>
2024-11-06 12:16:36 +0100

    iosevka

--- wezterm.lua
+++ wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if wezterm.target_triple:find("windows") ~= nil then
-	-- config.default_domain = "WSL:Arch"
 	-- config.win32_system_backdrop = "Acrylic"
+    config.default_domain = "WSL:Arch"
 	config.font_size = 11
 elseif wezterm.target_triple:find("darwin") ~= nil then
 	config.font_size = 16
@@ -13,7 +13,7 @@ elseif wezterm.target_triple:find("linux") ~= nil then
 end
 
 config.color_scheme = "catppuccin-mocha"
--- config.font = wezterm.font("Inconsolata")
+config.font = wezterm.font("Iosevka Term")
 -- config.window_background_opacity = 0.7
 config.audible_bell = "Disabled"
 
