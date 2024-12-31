8510bcac1951a3f2745379b9720129e9555768af
kmate19 <kissmate019@gmail.com>
2024-12-31 11:29:54 +0100

    win

--- wezterm.lua
+++ wezterm.lua
@@ -5,20 +5,12 @@ local config = wezterm.config_builder()
 if wezterm.target_triple:find("windows") ~= nil then
 	config.default_domain = "WSL:Arch"
 	config.font_size = 12
-	config.win32_system_backdrop = "Acrylic"
-	config.window_background_opacity = 0.5
-elseif wezterm.target_triple:find("darwin") ~= nil then
-	config.font_size = 17
-	config.window_background_opacity = 0.5
-	config.macos_window_background_blur = 50
-elseif wezterm.target_triple:find("linux") ~= nil then
-	config.font_size = 12
 end
 
 config.enable_kitty_graphics = true
 config.max_fps = 120
 config.animation_fps = 120
-config.color_scheme = "Oxocarbon Dark (Gogh)"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.audible_bell = "Disabled"
 
 return config
