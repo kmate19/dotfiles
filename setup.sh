5c18abcbaea2e46fe0698b31934d5c52b466d0e2
kmate19 <kissmate019@gmail.com>
2024-12-31 11:48:41 +0100

    remove unused

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ $XDG_CONFIG_HOME ]]; then
-  ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-  ln -s ~/dotfiles/.zshrc ~/.zshrc
-  ln -s ~/dotfiles/kitty.conf $XDG_CONFIG_HOME/kitty/kitty.conf
 else
 fi
