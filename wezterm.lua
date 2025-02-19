2ddfe733255ec11bcc65c464cf02aa5e54b6d13b
kmate19 <kissmate019@gmail.com>
2025-02-19 12:42:28 +0100

    colro
    asd

--- wezterm.lua
+++ wezterm.lua
@@ -9,11 +9,12 @@ elseif wezterm.target_triple:find("darwin") then
 	config.font_size = 16
 end
 
+config.default_cursor_style = "BlinkingBar"
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.enable_kitty_graphics = true
 config.max_fps = 120
 config.animation_fps = 120
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "zenwritter_dark"
 config.audible_bell = "Disabled"
 
 return config
