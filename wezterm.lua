e96376fbeef575f9ffa0da731b6bbc0a717f2794
kmate19 <kissmate019@gmail.com>
2025-06-21 12:04:43 +0200

    fix: update default program for Windows to pwsh.exe and set font to Iosevka

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if wezterm.target_triple:find("windows") ~= nil then
-	config.default_domain = "WSL:Arch"
+	config.default_prog = {"pwsh.exe"}
 	config.font_size = 12
 elseif wezterm.target_triple:find("darwin") then
 	config.font_size = 16
@@ -11,6 +11,7 @@ end
 
 config.default_cursor_style = "BlinkingBar"
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.font = wezterm.font("Iosevka")
 config.enable_kitty_graphics = true
 config.max_fps = 120
 config.animation_fps = 120
