c4f32c903b7f55b64fa9dd69f0c4bf1632f6cc41
kmate19 <99414737+kmate19@users.noreply.github.com>
2025-08-29 13:37:04 +0200

    fix pwsh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,6 @@
 function v { nvim @args }
 function v. { nvim . }
 function gwip { git add -A; git rm $(git ls-files --deleted) 2>$null; git commit -m $(Read-Host "Commit message") }
-function gc { git commit @args }
 function gcl { git clone @args }
 function gs { git status @args }
 function gp { git push @args }
@@ -14,14 +13,16 @@ function ga { git add @args }
 function c { code @args }
 function pn { pnpm @args }
 function g { git @args }
-function l { eza -lah --icons }
+function l { eza -lah --icons @args }
 
 Remove-Item Alias:cd -Force
 Remove-Item Alias:cat -Force
 Remove-Item Alias:ls -Force
+Remove-Item Alias:gc -Force
 
 function cat { bat @args }
-function ls { eza --icons }
+function gc { git commit @args }
+function ls { eza --icons @args }
 function cd { z @args }
 
 Invoke-Expression (&starship init powershell)
